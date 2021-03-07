3492f0187be04009a9337200fc5ae689384c654c
sidmrlazy <59534733+sidmrlazy@users.noreply.github.com>
2021-03-07 10:33:52 +0530

    Added Publication Top Content Page

--- publication.php
+++ publication.php
@@ -1,4 +1,4 @@
 <?php include 'includes/main/header.php' ?>
 <?php include 'includes/main/navigation.php' ?>
-
+<?php include 'includes/publication/publication-top-content.php' ?>
 <?php include 'includes/main/footer.php' ?>
